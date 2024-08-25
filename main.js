799526b1e9ca6450821c9c7dd07ef1a26b73c914
shreyanshraj <shreyanshraj@gmail.com>
2024-08-25 15:04:30 -0700

    final draft minus dk_btn

--- main.js
+++ main.js
@@ -2,4 +2,13 @@ function changeTheme(){
     console.log("change theme");
     var element = document.body;
     element.classList.toggle("dark-mode");
+    
+}
+
+function changeLogo(){
+    document.getElementsByClassName("dk_btn").addEventListener("click", function(e){
+        var target = e.target;
+
+        e.src("/icons/white-circle.svg")
+    },false);
 }
